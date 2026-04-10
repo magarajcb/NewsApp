cb9313b05009cd4da3cedfe6b089eecad56f5b96
magaraj <magarajcb@gmail.com>
2026-04-10 21:37:47 +0530

    data connected to DB

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,19 @@
+const user=require('../models/user')
 const authController={
 registerUser:async (req,res)=>{
     try{
+        const {name,email,password}=req.body;
+        const newUser=new user({
+            name,
+            email,
+            password
+        });
+        const savedUser=await newUser.save();
        
-return res.status(200).json({message:"Register user endpoint,sSucessfully!!"})
+return res.status(200).json({message:"Register user endpoint,Sucessfully!!",newUser:savedUser})
     }
     catch(error){
-
+console.log("Failed to connect DB")
     }
 }
 }
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
+    "name":String,
+    "email":String,
+    "password":String
 },{timestamps:true})
-module.exports=mongoose.model("users",userSchema,"user")
+module.exports=mongoose.model("user",userSchema,"test")
