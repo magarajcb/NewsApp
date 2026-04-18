4a595af96c432cdb47c3ac76b211e4f7be1653ee
magaraj <magarajcb@gmail.com>
2026-04-18 21:20:59 +0530

    frontend file setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello World</h1>
+        <p>Your MERN stack journey starts here.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
